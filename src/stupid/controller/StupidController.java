313e4fc1d724bf110633bee67bc005d10f31972c
Amber Bullough <amber@bertbullough.com>
2017-09-07 08:16:04 -0600

    Started Runner file and added Controller and runner

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -4,7 +4,7 @@ public class StupidController {
 	
 	public void start()
 		{
-			System.out.println("My name is Amber. I am a CIborg and I love to write. I am 17.");
+			System.out.println("My name is Amber. I am a CIborg and I love to write. I am 17 years old.");
 		}
 	}
 

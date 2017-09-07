6693bc98966b8d77d9324189c96dbacdc626881d
Amber Bullough <amber@bertbullough.com>
2017-09-07 08:14:08 -0600

    Initial commit and added .gitignore.

---
+++ src/stupid/controller/StupidController.java
@@ -0,0 +1,11 @@
+package stupid.controller;
+
+public class StupidController {
+	
+	public void start()
+		{
+			System.out.println("My name is Amber. I am a CIborg and I love to write. I am 17.");
+		}
+	}
+
+

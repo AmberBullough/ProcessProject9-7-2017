d4997215aa23e7783d96b80656afe65b603c424f
Amber Bullough <amber@bertbullough.com>
2017-09-11 08:26:53 -0600

    Added in PlayDohCircle in model and controllers

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,9 +1,18 @@
 package stupid.controller;
 
+import stupid.model.PlayDohCircle;
+
 public class StupidController {
-	
+
 	public void start()
-		{
+	{
+		PlayDohCircle firstCircle = new PlayDohCircle();
+		PlayDohCircle secondCircle;
+		secondCircle = new PlayDohCircle ();
+		PlayDohCircle thirdCircle;
+		thirdCircle = new PlayDohCircle ();
+	}
+	{
 			System.out.println("My name is Amber. I am a CIborg and I love to write. I am 17 years old.");
 		}
 	}
